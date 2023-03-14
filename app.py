10d1ec1c65ae181a1f8132589b24d271535674e8
nvquynh29 <nvquynh29@gmail.com>
2023-03-14 15:39:48 +0700

    init repository

---
+++ app.py
@@ -0,0 +1,40 @@
+import os, json
+from flask import Flask, request
+from flask_cors import CORS
+from dotenv import load_dotenv
+from dataset import to_tfrecord_dataset
+import rediswq
+
+app = Flask(__name__)
+CORS(app)
+load_dotenv()
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route('/clf/dataset', methods=['POST'])
+def create_clf_dataset():
+    data = request.get_json(force=True)
+    print(data, type(data))
+    to_tfrecord_dataset(data)
+    # {'name': 'Test', 'classes': ['ants', 'bees']} <class 'dict'>
+    return data
+
+pretrained_models = ['Xception', 'VGG16']
+def create_train_jobs(host, queue_name):
+    q = rediswq.RedisWQ(name=queue_name, host=host)
+    for model_name in pretrained_models:
+        job = json.dumps({ 'model_name': model_name, 'num_epochs': 2, 'dataset_url': 'https://google.com'})
+        q.put(job)
+
+@app.route('/clf/train', methods=['POST'])
+def train_model():
+    host = os.getenv('REDIS_ADDR')
+    queue_name = "mnist"
+    create_train_jobs(host, queue_name)
+    os.system(f"python3 worker.py --redis_host={host} --queue_name={queue_name} &")
+    return {'message': 'Training in progress'}, 202
+
+if __name__ == '__main__':
+    app.run(port=4000)
